02d41028f975e0756b09179c5a8e8a473bb6d85f
atharva-2001 <atharva-2001@users.noreply.github.com>
2023-08-29 09:14:46 +0000

    deploy: 8afecd4815b7485bc12ce22c1270d8ba0ca961ab

--- branch/docs_update/_static/documentation_options.js
+++ branch/docs_update/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev712+g73aa215',
+    VERSION: '0.1.dev713+g8afecd4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
